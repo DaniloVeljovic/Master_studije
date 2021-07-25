f3a70f3f0353ac26809d6531e2c59ddaec859ecb
DaniloVeljovic <danilo.theextinced@gmail.com>
2021-07-25 10:25:58 +0200

    started working on my master thesis

---
+++ Master rad/Projekat/BE/devicesservice/src/main/java/elfak/masterrad/devicesservice/DevicesserviceApplication.java
@@ -0,0 +1,13 @@
+package elfak.masterrad.devicesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevicesserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevicesserviceApplication.class, args);
+	}
+
+}
---
+++ Master rad/Projekat/BE/ingestionservice/src/main/java/elfak/masterrad/ingestionservice/IngestionserviceApplication.java
@@ -0,0 +1,13 @@
+package elfak.masterrad.ingestionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngestionserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngestionserviceApplication.class, args);
+	}
+
+}
