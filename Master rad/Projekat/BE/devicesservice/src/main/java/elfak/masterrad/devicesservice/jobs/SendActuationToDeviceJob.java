cc1588100b2492b6f309dcb2750b654798ea0683
DaniloVeljovic <danilo.theextinced@gmail.com>
2021-08-28 21:52:07 +0200

    contemplating testing

--- Master rad/Projekat/BE/devicesservice/src/main/java/elfak/masterrad/devicesservice/jobs/SendActuationToDeviceJob.java
+++ Master rad/Projekat/BE/devicesservice/src/main/java/elfak/masterrad/devicesservice/jobs/SendActuationToDeviceJob.java
@@ -3,18 +3,12 @@ package elfak.masterrad.devicesservice.jobs;
 import elfak.masterrad.devicesservice.models.ActuationStatus;
 import elfak.masterrad.devicesservice.models.entities.Actuation;
 import elfak.masterrad.devicesservice.services.ActuationService;
-import elfak.masterrad.devicesservice.services.impl.ActuationServiceImpl;
-import org.quartz.Job;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-
-import java.time.Instant;
 import java.util.Date;
 import java.util.List;
 
@@ -26,6 +20,9 @@ public class SendActuationToDeviceJob {
     @Autowired
     private ActuationService actuationService;
 
+    @Autowired
+    SimpMessagingTemplate template;
+
     @Scheduled(fixedRate = 5000)
     public void execute() {
         logger.info("Started execution of SendActuationToDeviceJob");
@@ -35,6 +32,7 @@ public class SendActuationToDeviceJob {
             actuationService.sendActuationToDevice(actuation);
             logger.info("Sent actuation message to device ");
             actuationService.saveActuation(actuation);
+            template.convertAndSend("/topic/message", actuation);
         }
     }
 }
